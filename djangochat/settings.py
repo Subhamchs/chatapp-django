d80ff6fcf1c2694e1f9a593b806f8fda436f06e7
Subham Choudhury <suvamchiku.sc@gmail.com>
2022-02-12 17:33:58 +0530

    Second commit

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -53,8 +53,15 @@ WSGI_APPLICATION = 'djangochat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTION': {
+            'init_command': "SET sql_mode=STRICT_TRANS_TABLES'"
+        }
     }
 }
 
